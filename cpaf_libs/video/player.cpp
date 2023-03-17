dfadb3d094520e4e5516677aa48c7e3301d272f9
Martin Lütken <martin@lutken.dk>
2023-03-17 12:36:31 +0100

    video::player initial multi source streams support

--- cpaf_libs/video/player.cpp
+++ cpaf_libs/video/player.cpp
@@ -7,5 +7,75 @@ player::player()
 
 }
 
+/// @todo need to support reading "hybrid" resources that (potentially) specify different streams
+///       for video, audio, key_frames etc.. For now we simply open a single primary stream with this.
+bool player::open(const std::string& resource_path)
+{
+    return open_primary_stream(resource_path);
+}
+
+bool player::has_source_stream(stream_type_t sti) const
+{
+    return source_stream(sti) == nullptr;
+}
+
+// TODO: A little convoluted this implementation
+const play_stream* player::source_stream(stream_type_t sti) const
+{
+    const auto index = to_size_t(sti);
+    if (source_streams_[index]) {
+        return source_streams_[index].get();
+    }
+    if (primary_source_stream_) {
+        if (sti == stream_type_t::video && primary_source_stream_->has_media_type(media_type::video)) {
+            return primary_source_stream_.get();
+        }
+        else if (sti == stream_type_t::audio && primary_source_stream_->has_media_type(media_type::audio)) {
+            return primary_source_stream_.get();
+        }
+        else if (sti == stream_type_t::subtitle && primary_source_stream_->has_media_type(media_type::subtitle)) {
+            return primary_source_stream_.get();
+        }
+    }
+    return nullptr;
+}
+
+// TODO: A little convoluted this implementation
+play_stream* player::source_stream(stream_type_t sti)
+{
+    const auto index = to_size_t(sti);
+    if (source_streams_[index]) {
+        return source_streams_[index].get();
+    }
+    if (primary_source_stream_) {
+        if (sti == stream_type_t::video && primary_source_stream_->has_media_type(media_type::video)) {
+            return primary_source_stream_.get();
+        }
+        else if (sti == stream_type_t::audio && primary_source_stream_->has_media_type(media_type::audio)) {
+            return primary_source_stream_.get();
+        }
+        else if (sti == stream_type_t::subtitle && primary_source_stream_->has_media_type(media_type::subtitle)) {
+            return primary_source_stream_.get();
+        }
+    }
+    return nullptr;
+}
+
+bool player::open_stream(const std::string& resource_path, stream_type_t sti)
+{
+    const auto index = to_size_t(sti);
+    source_streams_[index] = std::make_unique<play_stream>();
+    const auto open_ok = source_streams_[index]->open(resource_path);
+
+    return open_ok;
+}
+
+bool player::open_primary_stream(const std::string& resource_path)
+{
+    primary_source_stream_ = std::make_unique<play_stream>();
+    const auto open_ok = primary_source_stream_->open(resource_path);
+    return open_ok;
+}
+
 } //END namespace cpaf::video
 
--- cpaf_libs/video/player.h
+++ cpaf_libs/video/player.h
@@ -1,6 +1,9 @@
-#ifndef CPAF_VIDEO_PLAYER_H
-#define CPAF_VIDEO_PLAYER_H
+#pragma once
 
+#include <array>
+#include <memory>
+#include <cpaf_libs/video/av_util.h>
+#include <cpaf_libs/video/play_stream.h>
 
 namespace cpaf::video {
 
@@ -8,9 +11,26 @@ class player
 {
 public:
     player();
+    bool                        open                    (const std::string& resource_path);
+
+    bool                        has_source_stream       (stream_type_t sti) const;
+    const play_stream*          source_stream           (stream_type_t sti) const;
+    play_stream*                source_stream           (stream_type_t sti);
+
+private:
+
+    bool                        open_stream             (const std::string& resource_path, stream_type_t sti);
+    bool                        open_primary_stream     (const std::string& resource_path);
+
+
+    // ----------------------------
+    // --- PRIVATE: Member vars ---
+    // ----------------------------
+//    std::array<play_stream*, stream_type_index_size()>                  source_stream_ptrs_ = {nullptr, nullptr, nullptr, nullptr, nullptr};
+    std::array<std::unique_ptr<play_stream>, stream_type_index_size()>  source_streams_ = {nullptr, nullptr, nullptr, nullptr, nullptr};
+    std::unique_ptr<play_stream>                                        primary_source_stream_;
+
 };
 
 } //END namespace cpaf::video
 
-
-#endif // CPAF_VIDEO_PLAYER_H
