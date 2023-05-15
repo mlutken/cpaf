189759003a36658cdca93c19ff2b274c80a108da
Martin Lütken <martin@lutken.dk>
2023-05-15 23:26:53 +0200

    Removed luepp

--- cul/luepp/tools/cmake/set_platform_compiler_defines.cmake
+++
@@ -1,6 +0,0 @@
-add_definitions(
-    -DLUEPP_PLATFORM_TYPE_ID=${LUEPP_PLATFORM_TYPE_ID}
-    -DLUEPP_SYSTEM_ID=${LUEPP_SYSTEM_ID}
-)
-
-message ("LUEPP_SYSTEM_NAME          : '${LUEPP_SYSTEM_NAME}'")
