f23d7d120ab1b95936c9ec76c69e98c28ffb054c
Martin Lütken <martin@lutken.dk>
2023-05-15 23:27:06 +0200

    Squashed 'cul/luepp/' content from commit 2c1b2a1
    git-subtree-dir: cul/luepp
    git-subtree-split: 2c1b2a1049d9e9661a24375d09e37fe5e2d14933

---
+++ tools/cmake/set_platform_compiler_defines.cmake
@@ -0,0 +1,6 @@
+add_definitions(
+    -DLUEPP_PLATFORM_TYPE_ID=${LUEPP_PLATFORM_TYPE_ID}
+    -DLUEPP_SYSTEM_ID=${LUEPP_SYSTEM_ID}
+)
+
+message ("LUEPP_SYSTEM_NAME          : '${LUEPP_SYSTEM_NAME}'")
