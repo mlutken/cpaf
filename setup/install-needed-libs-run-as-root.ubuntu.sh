902309d40509318d134f69509ea8d19098686e27
Martin Lütken <martin@lutken.dk>
2023-05-16 18:13:03 +0200

    Added setup folder

---
+++ setup/install-needed-libs-run-as-root.ubuntu.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+SAVE_DIR_CPAF_SETUP=`pwd`
+
+sudo apt-get install -y libx11-dev
+sudo apt-get install -y libx11-xcb-dev
+sudo apt-get install -y libfontenc-dev
+sudo apt-get install -y libice-dev
+sudo apt-get install -y libsm-dev
+sudo apt-get install -y libxau-dev
+sudo apt-get install -y libxaw7-dev
+sudo apt-get install -y libxcb-cursor-dev
+
+cd $SAVE_DIR_CPAF_SETUP
+
