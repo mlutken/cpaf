4cefe33ee58c0f117be59d6d92aaa30933c77b81
Martin Lütken <martin@lutken.dk>
2023-05-17 18:51:53 +0200

    Use libtorrent from Conan

--- build_3rdparty_cpaf.sh
+++ build_3rdparty_cpaf.sh
@@ -1,130 +1,130 @@
 #!/bin/bash
-cpaf_thirdparty_startup_dir=`pwd`
+#cpaf_thirdparty_startup_dir=`pwd`
 
-CPAF_VERSION_MAJOR=1
-CPAF_VERSION_MINOR=0
+#CPAF_VERSION_MAJOR=1
+#CPAF_VERSION_MINOR=0
 
-cpaf_root_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cpaf_install_dir="${HOME}/cpaf/${CPAF_VERSION_MAJOR}.${CPAF_VERSION_MINOR}/install"
-root_build_dir="${HOME}/cpaf/${CPAF_VERSION_MAJOR}.${CPAF_VERSION_MINOR}/build"
-base_external_sources_dir="${HOME}/cpaf/${CPAF_VERSION_MAJOR}.${CPAF_VERSION_MINOR}/external_sources"
-webcc_source_dir="${cpaf_root_dir}/cul/webcc"
+#cpaf_root_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+#cpaf_install_dir="${HOME}/cpaf/${CPAF_VERSION_MAJOR}.${CPAF_VERSION_MINOR}/install"
+#root_build_dir="${HOME}/cpaf/${CPAF_VERSION_MAJOR}.${CPAF_VERSION_MINOR}/build"
+#base_external_sources_dir="${HOME}/cpaf/${CPAF_VERSION_MAJOR}.${CPAF_VERSION_MINOR}/external_sources"
+#webcc_source_dir="${cpaf_root_dir}/cul/webcc"
 
-if [ ! -d ${cpaf_install_dir} ];            then mkdir -p ${cpaf_install_dir}; fi
-if [ ! -d ${root_build_dir} ];              then mkdir -p ${root_build_dir}; fi
-if [ ! -d ${base_external_sources_dir} ];   then mkdir -p ${base_external_sources_dir}; fi
-
-
-# mkdir -p ${cpaf_root_dir}/../../_build
-# root_build_dir="$( cd ${cpaf_root_dir}/../../_build && pwd )"
-#
-# mkdir -p ${cpaf_root_dir}/../../_build/install/cpaf
-# cpaf_install_dir="$( cd ${cpaf_root_dir}/../../_build/install/cpaf && pwd )"
-
-# base_external_sources_dir=${HOME}/temp/cpaf_3rdparty
-
-echo "cpaf_install_dir          : ${cpaf_install_dir}"
-echo "webcc_source_dir          : ${webcc_source_dir}"
-echo "base_external_sources_dir : ${base_external_sources_dir}"
-echo "root_build_dir            : ${root_build_dir}"
+#if [ ! -d ${cpaf_install_dir} ];            then mkdir -p ${cpaf_install_dir}; fi
+#if [ ! -d ${root_build_dir} ];              then mkdir -p ${root_build_dir}; fi
+#if [ ! -d ${base_external_sources_dir} ];   then mkdir -p ${base_external_sources_dir}; fi
 
 
-# -----------------------------
-# --- Detect system bitwith ---
-# -----------------------------
-BITWIDTH=64
+## mkdir -p ${cpaf_root_dir}/../../_build
+## root_build_dir="$( cd ${cpaf_root_dir}/../../_build && pwd )"
+##
+## mkdir -p ${cpaf_root_dir}/../../_build/install/cpaf
+## cpaf_install_dir="$( cd ${cpaf_root_dir}/../../_build/install/cpaf && pwd )"
+
+## base_external_sources_dir=${HOME}/temp/cpaf_3rdparty
+
+#echo "cpaf_install_dir          : ${cpaf_install_dir}"
+#echo "webcc_source_dir          : ${webcc_source_dir}"
+#echo "base_external_sources_dir : ${base_external_sources_dir}"
+#echo "root_build_dir            : ${root_build_dir}"
 
 
-LINUX_DIST="linux"
-WIDTH=${BITWIDTH}
-JOBS=6
-
-# -------------------------------------
-# --- Parse command line parameters ---
-# -------------------------------------
-for i in $*
-do
-	case $i in
-        --jobs=*)
-        JOBS=`echo $i | sed 's/[-a-zA-Z0-9]*=//'`
-        ;;
-    	-h|--help)
-		echo "Options:"
-        echo "  --jobs=[6]"
-        echo "    Set to number of jobs to use when compiling"
-        echo " "
-		exit
-		;;
-    	--default)
-		DEFAULT=YES
-		;;
-    	*)
-                # unknown option
-		;;
-  	esac
-done
+## -----------------------------
+## --- Detect system bitwith ---
+## -----------------------------
+#BITWIDTH=64
 
 
-echo "LINUX_DIST        : $LINUX_DIST"
-echo "WIDTH             : $WIDTH"
-echo "JOBS              : $JOBS"
+#LINUX_DIST="linux"
+#WIDTH=${BITWIDTH}
+#JOBS=6
 
-# Ensure we have the external source directory created before we try to git clone 
-# anything there.
-if [ ! -d ${base_external_sources_dir} ]; then mkdir -p ${base_external_sources_dir}; fi
+## -------------------------------------
+## --- Parse command line parameters ---
+## -------------------------------------
+#for i in $*
+#do
+#	case $i in
+#        --jobs=*)
+#        JOBS=`echo $i | sed 's/[-a-zA-Z0-9]*=//'`
+#        ;;
+#    	-h|--help)
+#		echo "Options:"
+#        echo "  --jobs=[6]"
+#        echo "    Set to number of jobs to use when compiling"
+#        echo " "
+#		exit
+#		;;
+#    	--default)
+#		DEFAULT=YES
+#		;;
+#    	*)
+#                # unknown option
+#		;;
+#  	esac
+#done
 
-# -------------
-# --- webcc ---
-# -------------
-function build_webcc()
-{
-    build_dir=${root_build_dir}/webcc
-    mkdir -p ${build_dir}
 
-    cd ${build_dir}
-    rm -rf *
+#echo "LINUX_DIST        : $LINUX_DIST"
+#echo "WIDTH             : $WIDTH"
+#echo "JOBS              : $JOBS"
 
-    echo cmake -G"Unix Makefiles" -DCMAKE_INSTALL_PREFIX=${cpaf_install_dir} -DCMAKE_BUILD_TYPE=MinSizeRel -DWEBCC_ENABLE_LOG=1  -DWEBCC_LOG_LEVEL=0 -DWEBCC_ENABLE_SSL=1 -DWEBCC_ENABLE_GZIP=1 -DWEBCC_ENABLE_AUTOTEST=OFF -DWEBCC_ENABLE_UNITTEST=OFF -DWEBCC_ENABLE_EXAMPLES=ON ${webcc_source_dir}
+## Ensure we have the external source directory created before we try to git clone
+## anything there.
+#if [ ! -d ${base_external_sources_dir} ]; then mkdir -p ${base_external_sources_dir}; fi
 
-    cmake -G"Unix Makefiles" -DCMAKE_CXX_FLAGS=-fPIC -DCMAKE_INSTALL_PREFIX=${cpaf_install_dir} -DCMAKE_BUILD_TYPE=MinSizeRel -DWEBCC_ENABLE_LOG=1  -DWEBCC_LOG_LEVEL=0 -DWEBCC_ENABLE_SSL=1 -DWEBCC_ENABLE_GZIP=1 -DWEBCC_ENABLE_AUTOTEST=OFF -DWEBCC_ENABLE_UNITTEST=OFF -DWEBCC_ENABLE_EXAMPLES=ON ${webcc_source_dir} 
+## -------------
+## --- webcc ---
+## -------------
+#function build_webcc()
+#{
+#    build_dir=${root_build_dir}/webcc
+#    mkdir -p ${build_dir}
 
-    VERBOSE=0 make -j ${JOBS}
-    make install
-}
+#    cd ${build_dir}
+#    rm -rf *
 
-# ------------------
-# --- libtorrent ---
-# ------------------
-function build_libtorrent()
-{
-    source_dir="${base_external_sources_dir}/libtorrent"
-    build_dir=${root_build_dir}/libtorrent
-    mkdir -p ${build_dir}
+#    echo cmake -G"Unix Makefiles" -DCMAKE_INSTALL_PREFIX=${cpaf_install_dir} -DCMAKE_BUILD_TYPE=MinSizeRel -DWEBCC_ENABLE_LOG=1  -DWEBCC_LOG_LEVEL=0 -DWEBCC_ENABLE_SSL=1 -DWEBCC_ENABLE_GZIP=1 -DWEBCC_ENABLE_AUTOTEST=OFF -DWEBCC_ENABLE_UNITTEST=OFF -DWEBCC_ENABLE_EXAMPLES=ON ${webcc_source_dir}
 
-    if [ ! -d ${source_dir} ];
-    then
-        cd ${base_external_sources_dir}
-        git clone --recurse-submodules https://github.com/arvidn/libtorrent.git
-    else
-        cd ${source_dir}
-        git pull --recurse-submodules
-    fi
+#    cmake -G"Unix Makefiles" -DCMAKE_CXX_FLAGS=-fPIC -DCMAKE_INSTALL_PREFIX=${cpaf_install_dir} -DCMAKE_BUILD_TYPE=MinSizeRel -DWEBCC_ENABLE_LOG=1  -DWEBCC_LOG_LEVEL=0 -DWEBCC_ENABLE_SSL=1 -DWEBCC_ENABLE_GZIP=1 -DWEBCC_ENABLE_AUTOTEST=OFF -DWEBCC_ENABLE_UNITTEST=OFF -DWEBCC_ENABLE_EXAMPLES=ON ${webcc_source_dir}
 
-    cd ${build_dir}
-    rm -rf *
-    pwd
-    echo cmake -G"Unix Makefiles" -DCMAKE_INSTALL_PREFIX=${cpaf_install_dir} -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=OFF -DCMAKE_CXX_STANDARD=20 ${source_dir}
+#    VERBOSE=0 make -j ${JOBS}
+#    make install
+#}
+
+## ------------------
+## --- libtorrent ---
+## ------------------
+#function build_libtorrent()
+#{
+#    source_dir="${base_external_sources_dir}/libtorrent"
+#    build_dir=${root_build_dir}/libtorrent
+#    mkdir -p ${build_dir}
+
+#    if [ ! -d ${source_dir} ];
+#    then
+#        cd ${base_external_sources_dir}
+#        git clone --recurse-submodules https://github.com/arvidn/libtorrent.git
+#    else
+#        cd ${source_dir}
+#        git pull --recurse-submodules
+#    fi
+
+#    cd ${build_dir}
+#    rm -rf *
+#    pwd
+#    echo cmake -G"Unix Makefiles" -DCMAKE_INSTALL_PREFIX=${cpaf_install_dir} -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=OFF -DCMAKE_CXX_STANDARD=20 ${source_dir}
     
-    cmake -G"Unix Makefiles" -DCMAKE_INSTALL_PREFIX=${cpaf_install_dir} -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=OFF -DCMAKE_CXX_STANDARD=20 ${source_dir}
+#    cmake -G"Unix Makefiles" -DCMAKE_INSTALL_PREFIX=${cpaf_install_dir} -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=OFF -DCMAKE_CXX_STANDARD=20 ${source_dir}
 
-    VERBOSE=1 make -j ${JOBS}
-    make install
-}
-
-
-build_webcc
-build_libtorrent
+#    VERBOSE=1 make -j ${JOBS}
+#    make install
+#}
 
 
-cd ${cpaf_thirdparty_startup_dir}
+#build_webcc
+#build_libtorrent
+
+
+#cd ${cpaf_thirdparty_startup_dir}
 
--- cpaf_libs/torrent/playground/torrent_playground.cpp
+++ cpaf_libs/torrent/playground/torrent_playground.cpp
@@ -1,14 +1,60 @@
 #include <iostream>
-#include <vector>
+#include <thread>
+#include <chrono>
+
+#include <libtorrent/session.hpp>
+#include <libtorrent/session_params.hpp>
+#include <libtorrent/add_torrent_params.hpp>
+#include <libtorrent/torrent_handle.hpp>
+#include <libtorrent/alert_types.hpp>
+#include <libtorrent/magnet_uri.hpp>
+
 // https://stackoverflow.com/questions/52327290/linking-openssl-with-webassembly
+// https://github.com/arvidn/libtorrent
+// https://libtorrent.org/
+// https://libtorrent.org/tutorial-ref.html
 
+// https://snapcraft.io/orion-desktop
+// TEST Sintel: "magnet:?xt=urn:btih:19288989FD99A41AD24F30C23B0271E0A825AC5F&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969%2Fannounce&tr=udp%3A%2F%2F9.rarbg.to%3A2920%2Fannounce&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337&tr=udp%3A%2F%2Ftracker.internetwarriors.net%3A1337%2Fannounce&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.pirateparty.gr%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.cyberia.is%3A6969%2Fannounce"
 using namespace std;
-// using namespace boost::posix_time;
 
-int main()
+
+int main(int argc, char const* argv[]) try
 {
     cerr << "--- cpaf torrent1 playground ---\n";
+    if (argc != 2) {
+        std::cerr << "usage: " << argv[0] << " <magnet-url>" << std::endl;
+        return 1;
+    }
+    lt::settings_pack p;
+    p.set_int(lt::settings_pack::alert_mask, lt::alert_category::status
+              | lt::alert_category::error);
+    lt::session ses(p);
 
+    lt::add_torrent_params atp = lt::parse_magnet_uri(argv[1]);
+    atp.save_path = "."; // save in current dir
+    lt::torrent_handle h = ses.add_torrent(std::move(atp));
 
-    return 0;
+    for (;;) {
+        std::vector<lt::alert*> alerts;
+        ses.pop_alerts(&alerts);
+
+        for (lt::alert const* a : alerts) {
+            std::cout << a->message() << std::endl;
+            // if we receive the finished alert or an error, we're done
+            if (lt::alert_cast<lt::torrent_finished_alert>(a)) {
+                goto done;
+            }
+            if (lt::alert_cast<lt::torrent_error_alert>(a)) {
+                goto done;
+            }
+        }
+        std::this_thread::sleep_for(std::chrono::milliseconds(200));
+    }
+done:
+    std::cout << "done, shutting down" << std::endl;
+}
+catch (std::exception& e)
+{
+    std::cerr << "Error: " << e.what() << std::endl;
 }
