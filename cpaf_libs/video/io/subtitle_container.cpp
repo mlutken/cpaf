673e7bcdf1f0188c92dae7ed3e8c0cea29864f0e
Martin Lütken <martin@lutken.dk>
2024-01-23 17:25:21 +0100

    Fix compile error

--- cpaf_libs/video/io/subtitle_container.cpp
+++ cpaf_libs/video/io/subtitle_container.cpp
@@ -12,7 +12,8 @@ void subtitle_container::resource_path_set(const std::string& resource_path)
 
 bool subtitle_container::is_zip_archive() const
 {
-    return resource_path_.ends_with(".zip") || resource_path_.contains(".zip/");
+    return resource_path_.ends_with(".zip");
+//    return resource_path_.ends_with(".zip") || resource_path_.contains(".zip/");
 }
 
 
