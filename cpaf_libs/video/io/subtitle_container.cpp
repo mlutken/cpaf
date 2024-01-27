e779b0598e97baf5d58fc2a148bd6f8922809214
Martin Lütken <martin@lutken.dk>
2024-01-27 16:29:34 +0100

    subtitle_container start adding parser functions

--- cpaf_libs/video/io/subtitle_container.cpp
+++ cpaf_libs/video/io/subtitle_container.cpp
@@ -1,23 +1,98 @@
 #include "subtitle_container.h"
-#include <filesystem>
-#include <Zippy.hpp>
+#include <algorithm>
+#include <ranges>
 #include <fmt/format.h>
 
 #include <cpaf_libs/unicode/cpaf_u8string_utils.h>
-#include <cpaf_libs/crypto/crypto_hash.h>
-#include <cpaf_libs/filesystem/cpaf_special_dirs.h>
-#include <cpaf_libs/net/cpaf_http_convenience.h>
-#include <cpaf_libs/streams/cpaf_streams.h>
-#include <cpaf_libs/filesystem/cpaf_file_directory_functions.h>
 
 using namespace std;
-using namespace cpaf::crypto;
 using namespace cpaf::unicode;
-using namespace cpaf::filesystem;
 using namespace std::chrono;
 
 
 namespace cpaf::video {
+
+
+/**
+
+@see https://en.cppreference.com/w/cpp/ranges/split_view
+@see https://stackoverflow.com/questions/68756784/cannot-use-stdchronoparse-even-with-std-c2a-and-g-11
+@see https://github.com/HowardHinnant/date
+*/
+void subtitle_container::parse_srt_file_data(std::string_view data_string_view)
+{
+    using std::operator""sv;
+    constexpr auto newline{"\n"sv};
+
+//    auto line_beg = data_string_view.begin();
+//    auto line_end = data_string_view.begin();
+//    const auto end = data_string_view.end();
+
+//    while (line_beg < end) {
+//        line_end = std::find(line_beg, end, '\n');
+//        string_view line(line_beg, line_end);
+//    //string_view line(&(*line_beg), static_cast<size_t>(std::distance(line_beg, line_end)));
+//        fmt::println("Line: {}", line);
+
+//        line_beg = ++line_end;
+//    }
+
+
+
+    for (const auto word : std::views::split(data_string_view, newline)) {
+        // with string_view's C++23 range constructor:
+        string_view line(word);
+        fmt::println("LINE {}", line);
+        parse_line(line);
+    }
+    fmt::println("");
+
+}
+
+void subtitle_container::parse_line(std::string_view line)
+{
+
+}
+
+bool subtitle_container::parse_sequence_number(std::string_view line)
+{
+    long seq = -1;
+    try {
+        seq = std::stol(std::string(line));
+        if (seq > -1) {
+            cur_parse_frame_.sequence_number = seq;
+            return true;
+        }
+    } catch (...) {
+    }
+
+    return false;
+}
+
+bool subtitle_container::parse_presentation_times(std::string_view line)
+{
+//    using sys_seconds = std::chrono::time_point<std::chrono::system_clock, std::chrono::seconds>;
+//    sys_seconds tp;
+//    std::chrono::parse("%a, %d %b %Y %T %z", tp);
+
+//    std::chrono::parse();
+}
+
+bool subtitle_container::parse_subtitle_line(std::string_view line)
+{
+    if (line.empty()) {
+        parse_state_ = state_t::frame_done;
+        return true;
+    }
+    cur_parse_frame_.lines.push_back(std::string(line));
+    return true;
+}
+
+void subtitle_container::push_current_frame()
+{
+    subtitles_.push_back(std::move(cur_parse_frame_));
+    cur_parse_frame_ = frame_t{};
+}
 
 
 
--- cpaf_libs/video/io/subtitle_container.h
+++ cpaf_libs/video/io/subtitle_container.h
@@ -4,8 +4,6 @@
 #include <memory>
 #include <filesystem>
 
-#include <cpaf_libs/video/av_util.h>
-#include <cpaf_libs/video/io/subtitle_text_file_data.h>
 
 namespace Zippy {
 class ZipArchive;
@@ -24,24 +22,47 @@ class subtitle_container
 {
 public:
 
+    struct frame_t {
+        std::vector<std::string>        lines                   {};
+        std::chrono::microseconds       presentation_time       {0};
+        std::chrono::microseconds       presentation_time_end   {0};
+        uint32_t                        sequence_number         {0};
+    };
+
+
     subtitle_container() = default;
+
+    void                            parse_srt_file_data     (std::string_view data_string_view);
 
 
     // ----------------------
     // --- Info functions ---
     // ----------------------
+    void                            language_code_set		(std::string_view lc) { language_code_ = lc; }
+    const std::string&              language_code			() const { return language_code_; }
 
     // ----------------------
     // --- Data functions ---
     // ----------------------
+    const std::vector<frame_t>&     subtitles               () const { return subtitles_; }
 
     // --- ---
 
 private:
+    enum class state_t {new_frame, seq_num, ps_times, text, frame_done};
     // ------------------------
     // --- PRIVATE: Helpers ---
     // ------------------------
+    void                            parse_line              (std::string_view line);
+    bool                            parse_sequence_number   (std::string_view line);
+    bool                            parse_presentation_times(std::string_view line);
+    bool                            parse_subtitle_line     (std::string_view line);
+    void                            push_current_frame      ();
 
+    std::vector<frame_t>            subtitles_;
+    std::string                     language_code_;
+    frame_t                         cur_parse_frame_;
+    state_t                         parse_state_ = state_t::new_frame;
 };
 
 } // namespace cpaf::video
