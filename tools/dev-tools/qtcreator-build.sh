e83eb740520741b0652bbad05d7da65a698e5a10
Martin Lütken <martin@lutken.dk>
2024-02-17 18:07:41 +0100

    QtCreator build script updated

--- tools/dev-tools/qtcreator-build.sh
+++ tools/dev-tools/qtcreator-build.sh
@@ -16,7 +16,9 @@ this_script_source_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 sudo apt install -y qt6-base-dev qt6-base-private-dev qt6-declarative-dev qt6-declarative-private-dev qt6-tools-dev qt6-tools-private-dev qt6-scxml-dev qt6-documentation-tools libqt6core5compat6-dev qt6-tools-dev-tools qt6-l10n-tools qt6-shader-baker libqt6shadertools6-dev qt6-quick3d-dev qt6-quick3d-dev-tools libqt6svg6-dev libqt6quicktimeline6-dev libqt6serialport6-dev
 
 # Clang tools
+sudo apt install -y clang clangd libclang-dev
 sudo apt install -y clang-15 clangd-15 libclang-15-dev
+#sudo apt install -y clang-14 clangd-14 libclang-14-dev
 
 #  Install Qt Creator dependenciess
 sudo apt install -y libgl1-mesa-dev libvulkan-dev libxcb-xinput-dev libxcb-xinerama0-dev libxkbcommon-dev libxkbcommon-x11-dev libxcb-image0 libxcb-keysyms1 libxcb-render-util0 libxcb-xkb1 libxcb-randr0 libxcb-icccm4
