a315df9679437329e623be55f239ae83d44dce91
Martin Lütken <martin@lutken.dk>
2024-03-22 09:04:48 +0100

    Added curl class

--- cpaf_libs/net/cpaf_http_convenience.cpp
+++ cpaf_libs/net/cpaf_http_convenience.cpp
@@ -1,20 +1,28 @@
 #include "cpaf_http_convenience.h"
 
-////#include <boost/process.hpp>
+#include <iostream>
+
 
 using namespace std::chrono;
       
 namespace cpaf { namespace net {
 
-
 // -------------------------
 // --- "Private" helpers ---
 // -------------------------
-size_t curl_write_file_data_helper(void *ptr, size_t size, size_t nmemb, FILE *stream ) {
+static size_t curl_write_file_data_helper(void *ptr, size_t size, size_t nmemb, FILE *stream ) {
     size_t written;
     written = fwrite(ptr, size, nmemb, stream);
     return written;
 }
+
+static size_t curl_write_string_data_helper(void *contents, size_t size, size_t nmemb, void *userp)
+{
+    ((std::string*)userp)->append((char*)contents, size * nmemb);
+    return size * nmemb;
+}
+
+
 
 static CURLcode curl_do_http_download_file_impl(const std::string& sSrcUrl,
                                                 const std::string& sDstFilePath,
@@ -41,11 +49,227 @@ static CURLcode curl_do_http_download_file_impl(const std::string& sSrcUrl,
     return res;
 }
 
-static size_t curl_write_string_data_helper(void *contents, size_t size, size_t nmemb, void *userp)
+
+
+// ------------------
+// --- curl class ---
+// ------------------
+
+curl::curl()
 {
-    ((std::string*)userp)->append((char*)contents, size * nmemb);
-    return size * nmemb;
 }
+
+curl::~curl()
+{
+    cleanup();
+}
+
+bool curl::is_transferring() const
+{
+    const auto progr = state();
+    return progr == state_t::transferring;
+}
+
+bool curl::can_start_new() const
+{
+    const auto progr = state();
+    return progr != state_t::transferring && progr != state_t::cancel_requested;
+}
+
+CURLcode curl::download_file(const std::string& url, const std::string& dst_file_path)
+{
+    if (dst_file_path.empty()) { return CURLE_READ_ERROR; }
+    mode_ = mode_t::download;
+    last_curlcode_ = CURLE_FAILED_INIT;
+
+    if (!url.empty()) {
+        init(url);
+    }
+    write_file_ = fopen(dst_file_path.c_str(), "wb");
+    curl_easy_setopt(curl_, CURLOPT_WRITEFUNCTION, curl_write_file_data_helper);
+    curl_easy_setopt(curl_, CURLOPT_WRITEDATA, write_file_);
+
+    state_ = state_t::transferring;
+    last_curlcode_ = curl_easy_perform(curl_);
+    cleanup();
+    return last_curlcode_;
+}
+
+CURLcode curl::upload_file(const std::string& url, const std::filesystem::path& src_file_path, const std::string& field_name)
+{
+    last_curlcode_ = CURLE_FAILED_INIT;
+    if (src_file_path.empty()) { return CURLE_READ_ERROR; }
+    mode_ = mode_t::upload;
+
+    if (!url.empty()) {
+        init(url);
+    }
+
+    struct curl_slist* headerlist = nullptr;
+    struct curl_httppost* post = nullptr;
+    struct curl_httppost* last = nullptr;
+    static const char buf[] = "Expect:";
+
+    headerlist 	= curl_slist_append(headerlist, buf);
+
+    curl_formadd(&post, &last,
+                 CURLFORM_COPYNAME, field_name.c_str(),
+                 CURLFORM_FILE, src_file_path.string().c_str(),
+                 CURLFORM_CONTENTTYPE, "multipart/form-data", //
+                 CURLFORM_END);
+
+
+    curl_easy_setopt(curl_, CURLOPT_HTTPHEADER, headerlist);
+    curl_easy_setopt(curl_, CURLOPT_HTTPPOST, post );
+
+    state_ = state_t::transferring;
+    last_curlcode_ = curl_easy_perform(curl_);
+
+    cleanup();
+    curl_formfree(post);
+    curl_slist_free_all (headerlist);
+
+    return last_curlcode_;
+}
+
+std::string curl::download_string(const std::string& url)
+{
+    std::string read_buffer;
+    mode_ = mode_t::download;
+
+    if (!url.empty()) {
+        init(url);
+    }
+    curl_easy_setopt(curl_, CURLOPT_WRITEFUNCTION, curl_write_string_data_helper);
+    curl_easy_setopt(curl_, CURLOPT_WRITEDATA, &read_buffer);
+    curl_easy_setopt(curl_, CURLOPT_WRITEFUNCTION, curl_write_string_data_helper);
+
+    state_ = state_t::transferring;
+    last_curlcode_ = curl_easy_perform(curl_);
+    cleanup();
+    return read_buffer;
+}
+
+CURLcode curl::download_file()
+{
+    return download_file("", file_path_);
+}
+
+CURLcode curl::upload_file(const std::string& field_name)
+{
+    return upload_file("", file_path_, field_name);
+}
+
+std::string curl::download_string()
+{
+    return download_string("");
+}
+
+curl& curl::init_url(const std::string& url)
+{
+    init(url);
+    return *this;
+}
+
+void curl::cancel_transfer()
+{
+    state_ = state_t::cancel_requested;
+}
+
+int64_t curl::total_size_bytes() const
+{
+    return mode_ == mode_t::download ? total_download_size_ : total_upload_size_;
+}
+
+int64_t curl::current_size_bytes() const
+{
+    return mode_ == mode_t::download ? cur_download_size_ : cur_upload_size_;
+}
+
+float curl::progress() const
+{
+    const auto total = total_size_bytes();
+    if (total == 0) { return 0; }
+
+    const auto cur = current_size_bytes();
+    return static_cast<float>(cur) / static_cast<float>(total);
+}
+
+
+void curl::init(const std::string& url)
+{
+    cleanup();
+    state_ = state_t::ready;
+    curl_ = curl_easy_init();
+    set_default_options(url);
+}
+
+void curl::set_default_options(const std::string& url)
+{
+    if (curl_) {
+        curl_easy_setopt(curl_, CURLOPT_TIMEOUT, duration_cast<seconds>(timeout_).count());
+        curl_easy_setopt(curl_, CURLOPT_FOLLOWLOCATION, 1L); // enable redirect following
+        curl_easy_setopt(curl_, CURLOPT_URL, url.c_str() );
+
+
+        curl_easy_setopt(curl_, CURLOPT_NOPROGRESS, 0L);
+        curl_easy_setopt(curl_, CURLOPT_XFERINFODATA, this);
+        curl_easy_setopt(curl_, CURLOPT_XFERINFOFUNCTION, curl::curl_progress_callback);
+    }
+}
+
+void curl::cleanup()
+{
+    if (state_ == state_t::transferring) {
+        if (last_curlcode_ == CURLE_OK) {
+            state_ = state_t::ready;
+        }
+        else {
+            state_ = state_t::error;
+        }
+    }
+    if (curl_) {
+        curl_easy_cleanup(curl_);
+    }
+    if (write_file_) {
+        fclose(write_file_);
+    }
+    curl_ = nullptr;
+    write_file_ = nullptr;
+}
+
+size_t curl::curl_progress_callback(void *clientp,
+                                    curl_off_t dltotal,
+                                    curl_off_t dlnow,
+                                    curl_off_t ultotal,
+                                    curl_off_t ulnow)
+{
+    auto this_curl = static_cast<curl*>(clientp);
+    this_curl->total_download_size_ = dltotal;
+    this_curl->cur_download_size_ = dlnow;
+    this_curl->total_upload_size_ = ultotal;
+    this_curl->cur_upload_size_ = ulnow;
+
+    // Call user callback if any assigned!
+    if (this_curl->progress_cb_) {
+        bool cancel_requested = this_curl->progress_cb_(this_curl->progress());
+        if (cancel_requested) {
+            this_curl->state_ = state_t::cancel_requested;
+        }
+    }
+
+    // Handle cancel transfer.
+    if (this_curl->state_ == state_t::cancel_requested) {
+        this_curl->state_ = state_t::cancelled;
+        return 1;   // Note: Anything but 0 will cancel the current transfer!
+    }
+
+    return 0; // All is good - continue transfer
+}
+
+// ----------------------
+// --- Free functions ---
+// ----------------------
 
 std::string curl_do_http_download_to_string_impl(const std::string sSrcUrl,
                                                 std::chrono::milliseconds timeout)
@@ -56,10 +280,10 @@ std::string curl_do_http_download_to_string_impl(const std::string sSrcUrl,
 
     curl = curl_easy_init();
     if (curl) {
+        curl_easy_setopt(curl, CURLOPT_TIMEOUT, duration_cast<seconds>(timeout).count());
         curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L); // enable redirect following
         curl_easy_setopt(curl, CURLOPT_URL, sSrcUrl.c_str() );
         curl_easy_setopt(curl, CURLOPT_WRITEDATA, &readBuffer);
-        curl_easy_setopt(curl, CURLOPT_TIMEOUT, duration_cast<seconds>(timeout).count());
     //    curl_easy_setopt(curl, CURLOPT_HEADER, 1L);
 
         curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, curl_write_string_data_helper);
@@ -194,9 +418,10 @@ CURLcode curlPostUploadFileSimple(const std::string& sURL,
 		curl_formfree(post);
 		curl_slist_free_all (headerlist);
 // 		curl_slist_free_all (headers);
-	}
-	return res;
+    }
+    return res;
 }
+
 
 
 
--- cpaf_libs/net/cpaf_http_convenience.h
+++ cpaf_libs/net/cpaf_http_convenience.h
@@ -3,6 +3,8 @@
 #include <string>
 #include <filesystem>
 #include <chrono>
+#include <functional>
+#include <atomic>
 #include <curl/curl.h>
 
 
@@ -15,9 +17,103 @@ Functions include easy download of a http page to a std::string.
 */
 
 
-// -----------------------------
-// --- Types and Definitions ---
-// -----------------------------
+// ------------------
+// --- curl class ---
+// ------------------
+
+/**
+ * Class to hanlde up/download using cURL library.
+ * @see https://curl.se/libcurl/c/CURLOPT_XFERINFOFUNCTION.html
+ *
+ * Examples:
+ * ---------
+ * @code
+
+    cpaf::net::curl mycurl;
+    const std::string download_url = "https://my.domain.com/some_file.txt";
+    const std::filesystem::path local_download_path = "https://my.domain.com/some_file.txt";
+
+    auto cb = [](float progress) -> bool {
+        std::cerr << "Progress " << progress * 100 << " %\n";
+        return false; // NOTE: To cancel operation return true!
+    };
+
+
+    // 3 Different ways of downloading a file
+    // Note: The CURLOPT_FOLLOWLOCATION option is by default set to 1L
+    auto res = mycurl.timeout(timeout).download_file(download_url, local_download_path_);
+    res = mycurl.timeout(timeout).init_url(download_url).file_path(local_download_path_).opt(CURLOPT_FOLLOWLOCATION, 0L).download_file();
+    res = mycurl.timeout(timeout).init_url(download_url).file_path(local_download_path_).progress_callback(std::move(cb)).download_file();
+ * @endcode
+*/
+class curl {
+public:
+    using progress_cb = std::function<bool (float)>;
+    enum class state_t : uint8_t { ready, transferring, cancel_requested, cancelled, error };
+    enum class mode_t : uint8_t { download, upload };
+
+    curl();
+    ~curl();
+
+    const std::filesystem::path&    file_path           () const { return file_path_; }
+    std::chrono::milliseconds       timeout             () const { return timeout_; }
+    std::chrono::seconds            timeout_seconds     () const { return std::chrono::duration_cast<std::chrono::seconds>(timeout_); }
+    state_t                         state               () const { return state_; }
+    bool                            is_transferring     () const;
+    bool                            can_start_new       () const;
+    CURLcode                        last_curlcode       () const { return last_curlcode_; }
+
+    CURLcode                        download_file       (const std::string& url, const std::string& dst_file_path);
+    CURLcode                        upload_file         (const std::string& url, const std::filesystem::path& src_file_path, const std::string& field_name);
+    std::string                     download_string     (const std::string& url);
+
+
+    CURLcode                        download_file       ();
+    CURLcode                        upload_file         (const std::string& field_name);
+    std::string                     download_string     ();
+    curl&                           init_url            (const std::string& url);
+    curl&                           file_path           (std::filesystem::path p)       { file_path_ = std::move(p); return *this; }
+    curl&                           timeout             (std::chrono::milliseconds to)  { timeout_ = to; return *this; }
+    curl&                           progress_callback   (progress_cb&& cb)              { progress_cb_ = std::move(cb);  return *this; }
+    curl&                           progress_callback   (const progress_cb& cb)         { progress_cb_ = cb;  return *this; }
+
+    void                            cancel_transfer     ();
+
+    int64_t                         total_size_bytes    () const;
+    int64_t                         current_size_bytes  () const;
+    float                           progress            () const;
+
+    template<typename OPT, typename TYPE>
+    curl&                           opt          (OPT opt, TYPE value)
+    {
+        curl_easy_setopt(curl_, opt, value);
+        return *this;
+    }
+
+private:
+    static size_t curl_progress_callback(void *clientp,
+                                        curl_off_t dltotal,
+                                        curl_off_t dlnow,
+                                        curl_off_t ultotal,
+                                        curl_off_t ulnow);
+
+    void                        init                (const std::string& url);
+    void                        set_default_options (const std::string& url);
+    void                        cleanup             ();
+
+    CURL*                       curl_               = nullptr;
+    FILE*                       write_file_         = nullptr;
+    std::filesystem::path       file_path_          {};
+    std::chrono::milliseconds   timeout_            = std::chrono::seconds(60);
+    progress_cb                 progress_cb_        {nullptr};
+    std::atomic<state_t>        state_              = state_t::ready;
+    mode_t                      mode_               = mode_t::download;
+    CURLcode                    last_curlcode_      = CURLE_OK;
+    int64_t                     total_download_size_= 0;
+    int64_t                     total_upload_size_  = 0;
+    int64_t                     cur_download_size_  = 0;
+    int64_t                     cur_upload_size_    = 0;
+};
 
 // -----------------
 // --- Functions ---
