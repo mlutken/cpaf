fe3b041216aa0b39780afd6df02286e823b4e4b8
Martin Lütken <martin@lutken.dk>
2024-05-25 09:55:53 +0200

    Squashed 'cul/KZip/' content from commit 31df0fb
    git-subtree-dir: cul/KZip
    git-subtree-split: 31df0fbb848eacfccde01e0542b127a478cb45df

---
+++ tests/test-data-text.hpp
@@ -0,0 +1,20 @@
+//
+// Created by Kenneth Balslev on 10/06/2020.
+//
+
+#include <string>
+
+#ifndef ZIPPY_TEST_DATA_TEXT_HPP
+#define ZIPPY_TEST_DATA_TEXT_HPP
+
+const std::string txtdata =
+    "MISSION CONTROL I wouldn't worry too much about the computer. First of all, there is still a chance that he is right, despite your "
+    "tests, and if it should happen again, we suggest eliminating this possibility by allowing the unit to remain in place and seeing whether "
+    "or not it actually fails. If the computer should turn out to be wrong, the situation is still not alarming. The type of obsessional "
+    "error he may be guilty of is not unknown among the latest generation of HAL 9000 computers. It has almost always revolved around a "
+    "single detail, such as the one you have described, and it has never interfered with the integrity or reliability of the computer's "
+    "performance in other areas. No one is certain of the cause of this kind of malfunctioning. It may be over-programming, but it could "
+    "also be any number of reasons. In any event, it is somewhat analogous to human neurotic behavior. Does this answer your query?  "
+    "Zero-five-three-Zero, MC, transmission concluded.";
+
+#endif //ZIPPY_TEST_DATA_TEXT_HPP
